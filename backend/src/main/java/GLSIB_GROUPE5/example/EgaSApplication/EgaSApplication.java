943f14b5da5aac17cda5060bc69c2bd6d9ae2b06
Thierno-dtd <davidouthe2@gmail.com>
2024-01-04 22:00:41 +0100

    initialisation du projet

---
+++ backend/src/main/java/GLSIB_GROUPE5/example/EgaSApplication/EgaSApplication.java
@@ -0,0 +1,13 @@
+package GLSIB_GROUPE5.example.EgaSApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgaSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgaSApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/GLSIB_GROUPE5/example/EgaSApplication/EgaSApplicationTests.java
@@ -0,0 +1,13 @@
+package GLSIB_GROUPE5.example.EgaSApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgaSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
